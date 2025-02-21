aad1d7d3e0e81d2ff778586aef4e05a4b0233577
= <=>
2025-02-21 12:10:53 +0100

    Created get started section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import ListComponent from "./ListComponent";
 import { GoRocket } from "react-icons/go";
-
+import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className="grid grid-cols-4 gap-4 gap-x-12 p-10">
@@ -21,9 +21,9 @@ export default function Footer() {
       <section>
         <p className="font-semibold m-1">Connect</p>
         <span className="flex gap-x-2 m-0">
-          <GoRocket className="text-gray-600" size={20} />
-          <GoRocket className="text-gray-600" size={20} />
-          <GoRocket className="text-gray-600" size={20} />
+          <FaTwitter className="text-gray-600" size={20} />
+          <FaGithub className="text-gray-600" size={20} />
+          <FaLinkedin className="text-gray-600" size={20} />
         </span>
       </section>
     </footer>
--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -2,11 +2,14 @@ import SmallInfoCard from "./SmallInfoCard";
 import { GiAcid } from "react-icons/gi";
 import { GiAnchor } from "react-icons/gi";
 import { GiAmmoniteFossil } from "react-icons/gi";
+import { GiArmBandage } from "react-icons/gi";
+import { FaDog } from "react-icons/fa";
+import { BiFork } from "react-icons/bi";
 
 export default function InfoCard({ heading, subHeading, type }) {
   return (
     <div className="flex flex-col items-center p-4">
-      <div>
+      <div className="pb-5 items-center">
         <h1 className="text-slate-700 text-2xl font-bold">{heading}</h1>
         <h3 className="text-slate-600 text-xl">{subHeading}</h3>
       </div>
@@ -34,21 +37,21 @@ export default function InfoCard({ heading, subHeading, type }) {
       ) : (
         <div className="flex flex-row">
           <SmallInfoCard
-            icon={<GiAcid size={32} />}
-            heading={"sadasda"}
+            icon={<GiArmBandage size={32} />}
+            heading={"Great feature"}
             subHeading={
-              "You can always trust us with your money we make the coolest experimental feature"
+              "Sell us your bodyparts we pa the best prices in business industry"
             }
           ></SmallInfoCard>
           <SmallInfoCard
-            icon={<GiAnchor size={32} />}
-            heading={"Selling boat"}
-            subHeading={"We also sell som eboats sometimes i think"}
+            icon={<FaDog size={32} />}
+            heading={"Cool dog"}
+            subHeading={"I once saw a cool dog theres a picture of him above"}
           ></SmallInfoCard>
           <SmallInfoCard
-            icon={<GiAmmoniteFossil size={32} />}
-            heading={"Found some cool shells"}
-            subHeading={"I found some cool shells that i wanted to show"}
+            icon={<BiFork size={32} />}
+            heading={"Gaffel"}
+            subHeading={"Gaffel"}
           ></SmallInfoCard>
         </div>
       )}
